883598c5d3e91e9acc2dbd27b0db06374009e08c
Raihan Momtaz <63046447+Raihan-Momtaz@users.noreply.github.com>
2024-04-07 20:57:30 +1000

    search component added

--- car-checkmate-frontend/src/pages/scripts/CarInfoPage.jsx
+++ car-checkmate-frontend/src/pages/scripts/CarInfoPage.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import '../styles/CarInfoPage.css';
 import Navbar from '../../components/scripts/navbar';
-
+import SearchBar from '../../components/scripts/searchbar';
 
 export default function CarInfoPage() {
     return(
-        <div>
+        <div className="container">
             <Navbar/>
+            <SearchBar/>
             <h1>Cars</h1>
         </div>
     )
