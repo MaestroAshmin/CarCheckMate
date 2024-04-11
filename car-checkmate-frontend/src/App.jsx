95ed0b1c8e1893a722a253dd5ec440ea17ff2821
Raihan Momtaz <63046447+Raihan-Momtaz@users.noreply.github.com>
2024-04-12 01:25:02 +1000

    sign-in page completed

--- car-checkmate-frontend/src/App.jsx
+++ car-checkmate-frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import LandingPage from './pages/scripts/LandingPage';
 import CarInfoPage from './pages/scripts/CarInfoPage';
 import SignUpPage from './pages/scripts/SignUpPage'
+import SignInPage from './pages/scripts/SignInPage'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           <Route path="/" element={<LandingPage />} />
           <Route path="/CarInfoPage" element={<CarInfoPage />} />
           <Route path="/SignUpPage" element= {<SignUpPage/>} />
+          <Route path="/SignInPage" element= {<SignInPage/>} />
         </Routes>
       </BrowserRouter>
     </div>
