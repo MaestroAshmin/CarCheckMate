462ee43669691da94493d55197ca1a84f3373fc5
Inwoo-Bang <104250100@student.swin.edu.au>
2024-04-25 21:37:47 +1000

    Update App.jsx

--- car-checkmate-frontend/src/App.jsx
+++ car-checkmate-frontend/src/App.jsx
@@ -9,7 +9,7 @@ import CarPhotoPage from './pages/scripts/CarPhotoPage';
 import CarLocationPage from './pages/scripts/CarLocationPage';
 import CarPricePage from './pages/scripts/CarPricePage';
 import CarInfoPage from './pages/scripts/Listing/CarInfoPage';
-import CarPostPage from './pages/scripts/Listing/CarPostPage';
+// import CarPostPage from './pages/scripts/Listing/CarPostPage';
 import Mechanic from './pages/scripts/Mechanic';
 import InspectionReport from './pages/scripts/InspectionReport';
 import StyleTest from './pages/scripts/StyleTest';
@@ -21,18 +21,21 @@ function App() {
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/Homepage" element={<Homepage />} />
+
           {/* Car listing */}
-          <Route path="/car/:carId" element={<CarInfoPage />} />
-          <Route path="/listing" element={<CarPostPage />} />
+          <Route path="/car/:_id" element={<CarInfoPage />} />
+          {/* <Route path="/listing" element={<CarPostPage />} /> */}
           <Route path="/CarAdPage" element={<CarAdPage />} />
           <Route path="/CarBuildPage" element={<CarBuildPage />} />
           <Route path="/CarLocationPage" element={<CarLocationPage />} />
           <Route path="/CarPhotoPage" element={<CarPhotoPage />} />
           <Route path="/CarFeaturePage" element={<CarFeaturePage />} />
           <Route path="/CarPricePage" element={<CarPricePage />} />
+
           {/* Login Page for each user */}
           <Route path="/Mechanic" element={<Mechanic />} />
           <Route path="/InspectionReport" element={<InspectionReport />} />
+          
           {/* Testing styling */}
           <Route path="/StyleTest" element={<StyleTest />} />
         </Routes>
