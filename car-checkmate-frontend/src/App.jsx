aa442a9d3176322caea8812548bae26a22d69ad5
Inwoo-Bang <104250100@student.swin.edu.au>
2024-04-25 21:52:16 +1000

    Update App.jsx

--- car-checkmate-frontend/src/App.jsx
+++ car-checkmate-frontend/src/App.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/scripts/LandingPage';
 import Homepage from './pages/scripts/Homepage';
