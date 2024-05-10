350960b6890d13734a995bdf4cdc448418038fdd
jayspm <jayispm@gmail.com>
2024-05-10 17:54:55 +1000

    start fixing the css for user content

--- car-checkmate-frontend/src/components/scripts/MechanicSchedule.jsx
+++ car-checkmate-frontend/src/components/scripts/MechanicSchedule.jsx
@@ -78,13 +78,19 @@ useEffect(() => {
           <img src={schedule.car.carPhotos[0]} alt="Car" />
           <div className='ctr-schedule-buyer-detail'>
           
-          
             <h3>Car: <span>{schedule.car.make}</span> <span>{schedule.car.model}</span></h3>
             {/* <p>Name: <span>{schedule.name}</span></p> */}
             
-            
-            <p>Date: <span>{schedule.inspectionDate.slice(0, 10)}</span><br/> <br/>  Time: <span>{schedule.inspectionTime}</span></p>
-            <p>Location:<span>{schedule.car.streetName}</span> <span>{schedule.car.suburb} </span> <span>{schedule.car.state} </span><span>{schedule.car.postcode} </span></p>
+            <p>
+                Date: <span>{schedule.inspectionDate.slice(0, 10)}</span>
+                <br/><br/>
+                Time: <span>{schedule.inspectionTime}</span>
+            </p>
+            <p>Location:&nbsp;
+              <span>{schedule.car.streetName}</span><br />
+              <span>{schedule.car.suburb}</span>, <span>{schedule.car.state}</span>
+              <span>{schedule.car.postcode} </span>
+            </p>
           </div>
           <div className='ctr-schedule-option'>
             <button onClick={openEmailBuyerPopup}>Email Buyer</button>
